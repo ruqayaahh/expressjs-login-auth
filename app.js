185611718fd38382f8ffef58a0ff41e2e5be1820
ruqayaah <49168086+ruqayaahh@users.noreply.github.com>
2021-03-09 10:36:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -42,9 +42,7 @@ const users = [
   },
 ];
 
-
 app.get("/users", (req, res) => res.status(200).json(users));
-
 
 // when registering
 app.post("/users/register", (req, res) => {
@@ -64,50 +62,64 @@ app.post("/users/register", (req, res) => {
   users.push(req.body);
   res.status(201).json({
     status: "Successful",
-    message: "Your account has been successfully created"
-  })
+    message: "Your account has been successfully created",
+  });
 });
 
 app.use("/users/:email", (req, res, next) => {
   const { email } = req.params;
   const currentUser = users.find((el) => el.email === email);
-  if(currentUser) {
+  if (currentUser) {
     req.data = currentUser;
     return next();
   }
   res.status(404).json({
     status: "Failed",
-    message: 'Account does not exist. Kindly login'
+    message: "Account does not exist. Kindly login",
   });
 });
 // when logging in
-app.get("/users/:email", (req, res) => { 
+app.get("/users/:email", (req, res) => {
   const { password } = req.body;
-  if(currentUser.password === password) {
+  if (currentUser.password === password) {
     req.body = { email, password };
+    req.data = { firstname, lastname, email, password }
+    if(req.body.firstname === req.data.lastname && req.body.lastname === req.data.lastname){
     return res.status(200).json({
       status: "Successfully logged in",
       message: "User fetched successfully",
-      data: req.body
+      data: req.body,
     });
   }
   res.status(401).json({
     status: "Unauthorized login",
-    message: "Email or password is incorrect"
-  })
-});
+    message: "Email or password is incorrect",
+  });
+};
 
 // when editing password
 app.put("/users/:email", (req, res) => {
-  const currentUser = { ...req.data, password: req.body.password};
-  const index = users.findIndex((el) => el.email === email)
-    users[index] = currentUser;
-    res.status(200).json({
-        status: "Successful changed password",
-        message: `Password updated successfully`,
-        data: currentUser,
-    });
+  const currentUser = { ...req.data, password: req.body.password };
+  const index = users.findIndex((el) => el.email === email);
+  users[index] = currentUser;
+  res.status(200).json({
+    status: "Successful changed password",
+    message: `Password updated successfully`,
+    data: currentUser,
   });
+});
+
+// when editing firstname
+app.put("/users/:email", (req, res) => {
+  const currentUser = { ...req.data, firstname: req.body.firstname };
+  const index = users.findIndex((el) => el.email === email);
+  users[index] = currentUser;
+  res.status(200).json({
+    status: "Successful changed firstname",
+    message: `Password updated firstname`,
+    data: currentUser,
+  });
+});
 
 
 app.listen(3000);
